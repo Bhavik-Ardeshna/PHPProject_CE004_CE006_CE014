d2c94dcc6b0566f86461b4e49037a903b14d1903
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-25 18:58:50 +0530

    addes static contactus page with css need to add db

--- about.php
+++ about.php
@@ -3,7 +3,8 @@ $title = "About Us";
 require_once 'includes/header.php';
 ?>
 
-<h1>About Us...</h1>
+
+
 
 <?php
 require_once 'includes/footer.php';
--- contact.php
+++ contact.php
@@ -3,7 +3,70 @@ $title = "Contact Us";
 require_once 'includes/header.php';
 ?>
 
-<h1>Contact Us...</h1>
+<link rel="stylesheet" href="./static/css/contactus.css">
+
+<!-- Component Code -->
+
+<div class="grid grid-flow-col grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 gap-4">
+
+    <form class="p-10">
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                    First Name
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+                <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+            </div>
+            <div class="w-full md:w-1/2 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                    Last Name
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    Email Address
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="********@*****.**">
+            </div>
+        </div>
+
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    Your Message
+                </label>
+                <textarea rows="10" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+
+      </textarea>
+            </div>
+            <div class="flex justify-between w-full px-3">
+                <div class="md:flex md:items-center">
+                    <label class="block text-gray-500 font-bold">
+                        <input class="mr-2 leading-tight" type="checkbox">
+                        <span class="text-sm">
+                            Send me your newsletter!
+                        </span>
+                    </label>
+                </div>
+                <button class="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded" type="submit">
+                    Send Message
+                </button>
+            </div>
+
+        </div>
+
+    </form>
+    <div class="w-full object-cover h-72 lg:w-full md:h-screen bg-cover bg-center" style="background-image:url(./static/img/contactus2.jpg);"></div>
+
+</div>
+
+
+
+
 
 <?php
 require_once 'includes/footer.php';
