96606b578bf78904d21681ed07a12d78e20492bb
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-25 19:28:10 +0530

    Added About us page but need  to change data

--- about.php
+++ about.php
@@ -3,6 +3,27 @@ $title = "About Us";
 require_once 'includes/header.php';
 ?>
 
+<link rel="stylesheet" href="./static/css/about.css">
+
+
+<section class="about">
+    <div class="about-img">
+        <img src="https://res.cloudinary.com/dxssqb6l8/image/upload/v1605293719/outdoor_tjjhxk.jpg">
+    </div>
+    <div class="about-text">
+        <small>ABOUT OUR COMPANY</small>
+        <h2>We are Go Trip Ravels Support Company</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud</p>
+
+        <label><input type="checkbox" checked>Lorem ipsum dolor sit amet</label>
+        <label><input type="checkbox" checked>consectetur adipisicing elit</label>
+        <label><input type="checkbox" checked>Architecto atque consequuntur</label>
+        <label><input type="checkbox" checked>cupiditate doloremque ducimus</label>
+        <a href="#">ABOUT US</a>
+    </div>
+</section>
 
 
 <?php
