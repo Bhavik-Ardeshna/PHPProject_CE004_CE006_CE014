3e027b5e233d879809e5274bb018b7a8bd38579d
birva-babaria <birvababaria@gmail.com>
2021-03-27 10:54:14 +0530

    hotel cards for user added

---
+++ agency/viewHotel.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+if (empty($_SESSION['AUTH_AGENCY_NAME'])) {
+    $_SESSION['ERROR_BEFORE_LOGIN_AGENCY'] = 'Login as Orgnisation Please!!';
+    header("Location: index.php");
+    exit();
+}
+?>
+
+<?php
+    $title = "Agency";
+    require_once '../includes/header.php';
+    require_once '../db/config.php';
+?>
+<link rel="stylesheet" href="../static/css/hotelform.css">
+<link rel="stylesheet" href="../static/css/home.css">
+<!-- <link rel="stylesheet" href="../static/css/login.css"> -->
+
+    <br><br>
+    <?php
+        $name = $_GET['n'];
+        $query = "SELECT * FROM hotel_data WHERE hotel_name='".$name."'";
+        $result = mysqli_query($conn,$query);
+        $data = mysqli_fetch_assoc($result);
+    ?>
+    <div class="hotel-info">
+        <h1 id='hotel-name'><?php echo $data['hotel_name']; ?></h1>
+        <br><br>
+        <div class="left">
+            <img src="<?php echo $data['hotelimg'] ?>">
+        </div>
+        <div class="right">
+            <div class="information">
+                <b>Email: </b><?php echo $data['email']; ?><br><br>
+                <b>Address: </b><?php echo $data['address']; ?><br><br>
+                <b>State: </b><?php echo $data['state']; ?><br><br>
+                <b>Contact no.: </b><?php echo $data['contactno']; ?><br><br>
+                <div id='status'>
+                    <b>*SingleBed rooms: </b><?php echo $data['singlestatus']; ?><br><br>
+                    <b>*DoubleBed rooms: </b><?php echo $data['doublestatus']; ?><br><br>
+                </div>
+            </div>
+        </div>
+        <br>
+        <?php if(isset($data['singleimg'])) { ?>
+        <div class="room-info">
+            <h2 id='room-heading'>*SingleBed room</h2>
+            <div class="left">
+                <img id="roomimg" src="<?php echo $data['singleimg'] ?>">
+            </div>
+            <div class="right">
+                <div class="information">
+                    <b>Price: </b><?php echo $data['singleprice']; ?>/-<br><br>
+                    <b>Available: </b><?php echo $data['singleavailable']; ?> rooms<br><br>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+
+        <?php if(isset($data['doubleimg'])) { ?>
+        <div class="room-info">
+            <h2 id='room-heading'>*DoubleBed room</h2>
+            <div class="left">
+                <img id="roomimg" src="<?php echo $data['doubleimg'] ?>">
+            </div>
+            <div class="right">
+                <div class="information">
+                    <b>Price: </b><?php echo $data['doubleprice']; ?>/-<br><br>
+                    <b>Available: </b><?php echo $data['doubleavailable']; ?> rooms<br><br>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+        <button class="edit-hotel-btn">
+            <a href="removeHotel.php?n=<?php echo $name ?>">Remove Hotel</a>
+        </button>
+    </div>
+
+<?php
+require_once '../includes/footer.php';
+?>
