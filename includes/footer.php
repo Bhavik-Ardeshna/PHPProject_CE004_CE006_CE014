61e8c38c18705d7bed99380fa0dee74d17a1443e
RiddhiChaudhari <ridchaudhari18@gmail.com>
2021-03-29 17:11:10 +0530

    Added FAQ and Tour

--- includes/footer.php
+++ includes/footer.php
@@ -2,25 +2,27 @@
     <div class="links">
         <h3>Quick Links</h3>
         <ul>
-            <li>Offers & Discounts</li>
-            <li>Get Coupon</li>
-            <li>Contact Us</li>
-            <li>About</li>
+            <li><a href="/PHPProject_CE004_CE006_CE014/contact.php">Contact Us</a></li>
+            <li><a href="/PHPProject_CE004_CE006_CE014/about.php">About</a></li>
+            <li><a href="/PHPProject_CE004_CE006_CE014/tour.php">Tour</a></li>
+            <li>Tour Guide</li>
+            <li>Private jet</li>
         </ul>
     </div>
     <div class="links">
-        <h3>New Products</h3>
+        <h3>Product Offering</h3>
         <ul>
-            <li>Woman Cloth</li>
-            <li>Fashion Accessories</li>
-            <li>Man Accessories</li>
-            <li>Rubber made Toys</li>
+            <li>Flights</li>
+            <li>Hotels</li>
+            <li>Cabs</li>
+            <li>Train</li>
         </ul>
     </div>
     <div class="links">
         <h3>Support</h3>
         <ul>
-            <li>Frequently Asked Questions</li>
+            <li><a href="/PHPProject_CE004_CE006_CE014/faqs.php">Frequently Asked Questions</a></li>
+            <li><a href="/PHPProject_CE004_CE006_CE014/user_agreement.php">User Agreement</a></li>
             <li>Report a Payment Issue</li>
             <li>Terms & Conditions</li>
             <li>Privacy Policy</li>
---
+++ user_agreement.php
@@ -0,0 +1,63 @@
+<?php
+$title = "About Us";
+require_once 'includes/header.php';
+?>
+
+<link rel="stylesheet" href="./static/css/useragreement.css">
+
+
+
+<div class="bg-gray-800 text-gray-100 px-8 py-12">
+    <div class="text-center w-full">
+        <svg class="text-gray-100 h-8 mx-auto" fill=currentColor viewBox="0 0 150 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="Desktop-HD" transform="translate(-1112.000000, -438.000000)" fill=currentColor>
+                    <g id="logo-4" transform="translate(1112.000000, 438.000000)">
+                    </g>
+                </g>
+            </g>
+        </svg>
+    </div>
+
+    <div class="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
+        <div class="flex flex-col  justify-between">
+            <h2 class="text-4xl lg:text-5xl font-bold leading-tight">Agreement between user and GO TRIP</h2>
+            <br>
+            <div class="description">
+                <table>
+                    <tr>
+                        <td><a id="1" name="1"></a>ELIGIBILITY TO USE</td>
+                    </tr>
+                    <tr>
+                        <td valign="top">
+                            <p>The User must be atleast 18 years of age and must possess the legal authority to enter into an agreement so as become a User. If you are a minor or are below the age of 18 years, you shall not register as a User of the Website and shall not transact on or use the Website.</p>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a id="2" name="2"></a>USER'S RESPONSIBILITY</td>
+                    </tr>
+                    <tr>
+                        <td valign="top">
+                            <p>If a User intends to make a booking on behalf of another person, it shall be the responsibility of the User to inform such person about the terms of this Agreement, including all rules and restrictions applicable thereto.</p>
+                            <br>
+                            <p>At the time of journy user must take care of their own luggage.Before the journy user must check their helth conditaion.</p>
+                            <br>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a id="3" name="3"></a>WEBSITE</td>
+                    </tr>
+                    <tr>
+                        <td valign="top">
+                            <p>Access to certain features of the Website may only be available to registered User(s). User represents and warrants that all information supplied is true and accurate</p>    
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+require_once 'includes/footer.php';
+?>
